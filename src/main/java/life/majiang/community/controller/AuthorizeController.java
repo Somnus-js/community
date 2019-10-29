47dc6502e80d0e99283a0c527efecae0d2a2e841
sfisnevermore <834118641@qq.com>
2019-10-29 13:52:09 +0800

    publish

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -48,7 +48,7 @@ public class AuthorizeController {
         GithubUser githubUser = githubProvider.getUser(accessToken);
         System.out.println(githubUser.getName());
 
-        if(githubUser != null){
+        if(githubUser != null && githubUser.getId() !=null){
 
             User user = new User();
             String token = UUID.randomUUID().toString();
@@ -58,6 +58,8 @@ public class AuthorizeController {
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            user.setAvatarUrl(githubUser.getAvatar_url());
+
             userMapper.insert(user);
             response.addCookie(new Cookie("token",token));
 
